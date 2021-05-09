6a4e2772526f162bac9326b0cff1b17f694bdf8f
Roman <vovkroman2013@gmail.com>
2021-05-09 07:33:32 +0200

    asdag

---
+++ src/js/script.js
@@ -0,0 +1,57 @@
+const data = {
+    "ua":
+    {
+        "name": 
+            "Лiлiя - майстер депиляцiї",
+        "work1":
+            "Подарую iдеальну красу",
+        "work2":
+            "Допоможу пiдiбрати домашнiй догляд",
+        "work3":
+            "Навчу мистецтву лазерної депіляції",
+        "pad":
+            "Чекаю на Вас в своєму затишному кабінеті за адресою",
+        "telegram__link":
+            "Записатися в",
+        "phone":
+            "Зателефонувати"
+
+    },
+    "ru":
+    {
+        "name": 
+            "Лилия - мастер депиляции",
+        "work1":
+            "Подарю идеальную красоту",
+        "work2":
+            "Помогу подобрать домашний уход",
+        "work3":
+            "Обучу искусству лазерной депиляции",
+        "pad":
+            "Жду Вас в своём уютном кабинете по адресу",
+        "telegram__link":
+            "Записаться в",
+        "phone":
+            "Позвонить"
+
+    },
+    "pl":
+    {
+        "name": 
+            "Lilia - mistrz depilacji",
+        "work1":
+            "Dam idealne piękno",
+        "work2":
+            "Pomogę Ci w wyborze opieki domowej",
+        "work3":
+            "Nauczę sztuki depilacji laserowej",
+        "pad":
+            "Czekam na Ciebie w moim przytulnym gabinecie przy ul",
+        "telegram__link":
+            "Zapisać się do",
+        "phone":
+            "Połączenie"
+
+    },
+        
+}
