a9bd38dfc9159e5cfa0bdbf3656993ce115e8703
Roman <vovkroman2013@gmail.com>
2021-05-09 08:13:15 +0200

    asdsdb

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,33 @@
-const data = {
+const langEl = document.querySelector(".lang__wrapper");
+const link = document.querySelectorAll(".lang__link");
+const translateItem1 = document.querySelector(".name");
+const translateItem2 = document.querySelector(".lang__link");
+const translateItem3 = document.querySelector(".work1");
+const translateItem4 = document.querySelector(".work2");
+const translateItem5 = document.querySelector(".work3");
+const translateItem6 = document.querySelector(".pad");
+const translateItem7 = document.querySelector(".telegram__link");
+const translateItem8 = document.querySelector(".phone");
+
+link.forEach(el => {
+    el.addEventListener('click', () => {
+        langEl.querySelector('.active').classList.remove('active');
+        el.classList.add('active');
+        
+        const attr = el.getAttribute('language');
+        translateItem1.textContent = data[attr].name;
+        translateItem2.textContent = data[attr].lang__link;
+        translateItem3.textContent = data[attr].work1;
+        translateItem4.textContent = data[attr].work2;
+        translateItem5.textContent = data[attr].work3;
+        translateItem6.textContent = data[attr].pad;
+        translateItem7.textContent = data[attr].telegram__link;
+        translateItem8.textContent = data[attr].phone;
+         
+    })
+});
+
+let data = {
     "ua":
     {
         "name": 
